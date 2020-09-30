fbd0816e4c84eef739747352da87ffbb8be91bad
Om Rawal <51584907+omrawal@users.noreply.github.com>
2020-09-30 23:40:10 +0530

    Update rough.py

--- rough.py
+++ rough.py
@@ -1,3 +1,5 @@
+# this type of array operation always creates problems so use a duplicate array or 
+# use while loop and increment by 2 just like we did in scheduling algorithms
 a=[1,2,3,4]
 for i in a:
     print(i)
